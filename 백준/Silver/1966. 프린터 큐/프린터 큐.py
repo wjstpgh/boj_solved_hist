1cd9f20b51005a042aa581365974118486bcc665
Seho Jeon <sehoJ3756@gmail.com>
2022-09-01 02:29:03 +0900

    [Silver III] Title: 프린터 큐, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,19 +1,14 @@
 case=int(input())
-
 for _ in range(case):
     n,m=map(int,input().split())
-    pq=list(map(int,input().split()))
-    impo=list(reversed(sorted(pq)))
-    im_idx,front=0,0
-    cnt=0
+    q=list(map(int,input().split()))
+    s=list(reversed(sorted(q)))
+    cnt,i=0,0
     while True:
-        if pq[front]==impo[im_idx]:
+        if q[i]==s[cnt]:
             cnt+=1
-            if front==m:
+            if i==m:
                 break
-            pq[front]=-1
-            im_idx+=1
-            
-        front=(front+1)%(n)
-        
+            q[i]=-1
+        i=(i+1)%n
     print(cnt)
