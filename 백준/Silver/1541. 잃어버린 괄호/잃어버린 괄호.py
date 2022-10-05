8a95d6ba64198d7d4bc23ff4d550e1ddb06a1984
Seho Jeon <sehoJ3756@gmail.com>
2022-10-06 00:17:49 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -2,7 +2,10 @@ sik=input()
 def das(arr):
     return sum(map(int,arr.replace('-','+').split('+')))
 try:
+    #첫번째 마이너스기호를 기준으로 문자열 나누기
     div=sik.index('-')
+    #나눠진 배열을 기호를 제거하여 숫자배열로 변경하여 합
     print(das(sik[:div])-das(sik[div+1:]))
 except:
+    #마이너스 기호가 없을 시 배열원소 다 더해주고 종료
     print(das(sik))
